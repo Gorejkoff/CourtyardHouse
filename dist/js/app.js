56b21c6a3dbdc5234c58758020489a41622aea26
gorejkoff <neskazua63@gmail.com>
2025-02-21 22:54:22 +0300

    add adaptive

--- dist/js/app.js
+++ dist/js/app.js
@@ -59,3 +59,148 @@ document.documentElement.addEventListener("click", (event) => {
 
 })
 
+
+// перемещение блоков при адаптиве
+// data-da=".class,3,768" 
+// класс родителя куда перемещать
+// порядковый номер в родительском блоке куда перемещается начиная с 0 как индексы массива
+// ширина экрана min-width
+// два перемещения: data-da=".class,3,768,.class2,1,1024"
+const ARRAY_DATA_DA = document.querySelectorAll('[data-da]');
+ARRAY_DATA_DA.forEach(function (e) {
+   const dataArray = e.dataset.da.split(',');
+   const addressMove = searchDestination(e, dataArray[0]);
+   const addressMoveSecond = dataArray[3] && searchDestination(e, dataArray[3]);
+   const addressParent = e.parentElement;
+   const listChildren = addressParent.children;
+   const mediaQuery = window.matchMedia(`(min-width: ${dataArray[2]}px)`);
+   const mediaQuerySecond = dataArray[5] && window.matchMedia(`(min-width: ${dataArray[5]}px)`);
+   for (let i = 0; i < listChildren.length; i++) { !listChildren[i].dataset.n && listChildren[i].setAttribute('data-n', `${i}`) };
+   mediaQuery.matches && startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray);
+   if (mediaQuerySecond && mediaQuerySecond.matches) moving(e, dataArray[4], addressMoveSecond);
+   mediaQuery.addEventListener('change', () => { startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray) });
+   if (mediaQuerySecond) mediaQuerySecond.addEventListener('change', () => {
+      if (mediaQuerySecond.matches) { moving(e, dataArray[4], addressMoveSecond); return; };
+      startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray);
+   });
+});
+
+function startChange(mediaQuery, addressMove, e, listChildren, addressParent, dataArray) {
+   if (mediaQuery.matches) { moving(e, dataArray[1], addressMove); return; }
+   if (listChildren.length > 0) {
+      for (let z = 0; z < listChildren.length; z++) {
+         if (listChildren[z].dataset.n > e.dataset.n) {
+            listChildren[z].before(e);
+            break;
+         } else if (z == listChildren.length - 1) {
+            addressParent.append(e);
+         }
+      }
+      return;
+   }
+   addressParent.prepend(e);
+};
+
+function searchDestination(e, n) {
+   if (e.classList.contains(n.slice(1))) { return e }
+   if (e.parentElement.querySelector(n)) { return e.parentElement.querySelector(n) };
+   return searchDestination(e.parentElement, n);
+}
+
+function moving(e, order, addressMove) {
+   if (order == "first") { addressMove.prepend(e); return; };
+   if (order == "last") { addressMove.append(e); return; };
+   if (addressMove.children[order]) { addressMove.children[order].before(e); return; }
+   addressMove.append(e);
+}
+
+
+
+// js-tabs-body - тело вкладки, в открытом состоянии добавляется класс js-tabs-open.
+// js-tabs-hover - работает hover на ПК, отключает клик на ПК, для touchscreen надо раставить js-tabs-click или js-tabs-toggle
+// js-tabs-closing - вместе с js-tabs-bod закрыть вкладку при событии вне данной вкладки
+// js-tabs-click - открыть при клике (зона клика)
+// js-tabs-toggle - открыть или закрыть при клике (зона клика)
+// js-tabs-shell - оболочка скрывающая js-tabs-inner
+// js-tabs-inner - оболочка контента
+// 
+// 
+//  работает в связке с определением touchscreen  (isPC)
+
+
+class Tabs {
+   constructor() {
+      this.listClosingTabs = document.querySelectorAll('.js-tabs-closing');
+      this.listHover = document.querySelectorAll('.js-tabs-hover');
+      this.listTabsBody = document.querySelectorAll('.js-tabs-body');
+   };
+   init = () => {
+      document.body.addEventListener('click', this.eventClick);
+      if (isPC) document.body.addEventListener('mouseover', this.eventMouseOver)
+      window.addEventListener('resize', this.resize);
+   };
+   eventMouseOver = (event) => {
+      if (event.target.closest('.js-tabs-hover')) this.openTabs(event);
+      this.closeAllHover(event);
+   };
+   eventClick = (event) => {
+      if (isPC && event.target.closest('.js-tabs-hover')) return;
+      this.closeAll(event);
+      if (event.target.closest('.js-tabs-click')) this.openTabs(event);
+      if (event.target.closest('.js-tabs-toggle')) this.toggleTabs(event);
+   };
+   openTabs = (event) => {
+      const body = event.target.closest('.js-tabs-body');
+      if (!body) return;
+      body.classList.add('js-tabs-open');
+      this.setHeight(body);
+   };
+   closeTabs = (body) => {
+      body.classList.remove('js-tabs-open');
+      this.clearHeight(body);
+   };
+   closeAll = (event) => {
+      const body = event.target.closest('.js-tabs-body');
+      if (this.listClosingTabs.length == 0 && body) return;
+      this.listClosingTabs.forEach((e) => { if (e !== body) this.closeTabs(e); })
+   };
+   closeAllHover = (event) => {
+      const body = event.target.closest('.js-tabs-hover');
+      if (this.listHover.length == 0 && body) return;
+      this.listHover.forEach((e) => { if (e !== body) this.closeTabs(e) })
+   };
+   setHeight = (body) => {
+      const heightValue = body.querySelector('.js-tabs-inner').offsetHeight;
+      body.querySelector('.js-tabs-shell').style.height = heightValue + "px";
+   };
+   clearHeight = (body) => { body.querySelector('.js-tabs-shell').style.height = "" }
+   toggleTabs = (event) => {
+      const body = event.target.closest('.js-tabs-body');
+      if (body.classList.contains('js-tabs-open')) {
+         this.closeTabs(body);
+         return;
+      }
+      this.openTabs(event);
+   };
+   throttle = () => {
+      let timer = null;
+      return () => {
+         if (timer) return;
+         timer = setTimeout(() => {
+            const unlocked = document.querySelectorAll('.js-tabs-open');
+            unlocked.forEach((e) => { this.setHeight(e) })
+            clearTimeout(timer);
+            timer = null;
+         }, 100)
+      }
+   };
+   resize = this.throttle();
+}
+const tabs = new Tabs();
+tabs.init();
+
+
+
+
+
+
